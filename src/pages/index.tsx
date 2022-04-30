b82287bbe4d0ff021855903a23a39b0124aac0e1
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-04-29 22:07:33 -0300

    feat: use static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "src/components/SubscribeButton";
 import { stripe } from "src/services/stripe";
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KtLIfCCwGTQBxZiOpUoP9NK");
 
   const product = {
@@ -51,5 +51,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { product },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
