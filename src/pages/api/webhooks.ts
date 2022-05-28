92e89e43e94d5b698d1b7dca76bfe480d7020386
Danilo Gonçalves <gonca.danilo@gmail.com>
2022-05-28 14:23:43 -0300

    feat: save subscription to database

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "src/services/stripe";
 import { Readable } from "stream";
 import Stripe from "stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -42,7 +43,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log("Evento recebido", event);
+      try {
+        switch (type) {
+          case "checkout.session.completed":
+            const { subscription, customer } = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              subscription.toString(),
+              customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch (error) {
+        return res.json({ error: "Webhook handler failed." });
+      }
     }
 
     res.json({ received: true });
