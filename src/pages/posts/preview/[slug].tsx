9aef311e2e49029ad0e95b51d048da631b086787
Danilo Gonçalves <danilo.santos@grupoboticario.com.br>
2022-06-13 21:42:31 -0300

    feat: create post preview page

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,101 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { RichText } from "prismic-dom";
+import { useEffect } from "react";
+import { getPrismicClient } from "src/services/prismic";
+import styles from "../post.module.scss";
+
+interface PostResponse {
+  title: string;
+  content: string;
+  last_publication_date: Date;
+}
+
+type Post = {
+  title: string;
+  slug: string;
+  content: string;
+  updatedAt: string;
+};
+
+interface PostPreviewProps {
+  post: Post;
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session]);
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+
+          <div
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID<PostResponse>(
+    "post",
+    String(slug),
+    {}
+  );
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.slice(0, 3)),
+    updateAt: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: { post },
+  };
+};
